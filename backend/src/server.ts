761634df0f4983d7fcf963e73fe4678fcfb7d6f8
Sergey Fomenko <s.fomenko@notamedia.com.ru>
2024-12-11 09:16:27 +0300

    рабочий докер для прода и дева

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,14 +2,12 @@ import { WebSocket } from 'ws';
 import { ENV } from './config';
 
 const PORT = parseInt(ENV.PORT) || 5000;
-const HOST = ENV.HOST || '127.0.0.1';
 
 export const wss = new WebSocket.Server(
 	{
 		port: PORT,
-		host: HOST,
 	},
-	() => console.log(`Server started on ${wss.options.host}:${wss.options.port}`),
+	() => console.log(`Server started on ${wss.options.port}`),
 );
 
 export function broadcastMessage(message: Record<string, any>) {
