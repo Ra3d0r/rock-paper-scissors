5e04146ffb112c4d6c3a443c363e330f2a90e994
Sergey Fomenko <s.fomenko@notamedia.com.ru>
2024-12-16 23:28:12 +0300

    добавлена валидация для ника игрока

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -7,7 +7,14 @@ export const Home = () => {
 	return (
 		<form onSubmit={onSubmit} className={styles.form}>
 			<h2>Введите любой ник для подключения к игре и нажмите Enter</h2>
-			<input type="text" name="username" className={styles.input} />
+			<input
+				type="text"
+				name="username"
+				className={styles.input}
+				required
+				pattern="^(?!\s*$).+"
+				title="Пожалуйста, введите текст, а не только пробелы"
+			/>
 			<button type="submit">Подключиться</button>
 		</form>
 	);
