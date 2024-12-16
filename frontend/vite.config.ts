5fec717ef04c3651c730a11a92926f803ceb2248
Sergey Fomenko <s.fomenko@notamedia.com.ru>
2024-12-16 23:17:09 +0300

    обновление конфига и документации

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+	const PORT = Number(env.VITE_PORT || 5173);
+
+	return {
+		plugins: [react()],
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+			},
 		},
-	},
+		server: {
+			port: PORT,
+		},
+	};
 });
