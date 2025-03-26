fe9af54b986fefba67fccde5b788021da8fd0168
Sergey Fomenko <fomenko_sergeg@mail.ru>
2025-03-26 21:35:10 +0300

    добавлен запуск prettier для всего проекта

--- apps/backend/eslint.config.js
+++ apps/backend/eslint.config.js
@@ -2,11 +2,11 @@ const js = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 
 module.exports = tseslint.config(
-  { ignores: ['dist', 'eslint.config.js'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    rules: {
-      ...tseslint.configs.recommended.rules,
-    },
-  },
+	{ ignores: ['dist', 'eslint.config.js'] },
+	{
+		extends: [js.configs.recommended, ...tseslint.configs.recommended],
+		rules: {
+			...tseslint.configs.recommended.rules,
+		},
+	},
 );
